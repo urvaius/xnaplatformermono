0c7fdb22d75f70e9eeac8292129c7de476461ac6
Dave Arneson <urvaius@gmail.com>
2013-09-06 17:06:19 -0700

    first commint on xnaplatfomer mono tutorial

---
+++ xnaplatformermono/GameScreen.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+
+namespace xnaplatformermono
+{
+   public class GameScreen
+    {
+
+       public virtual void Initialize()
+       {
+
+       }
+       public virtual void LoadContent(ContentManager Content)
+       {
+
+
+       }
+       public virtual void Update(GameTime gametime)
+       {
+
+       }
+       public virtual void Draw(SpriteBatch spriteBatch)
+       {
+       
+       }
+    }
+}
---
+++ xnaplatformermono/ScreenManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace xnaplatformermono
+{
+    public class ScreenManager
+    {
+        /// <summary>
+        /// Storing the GameScreens
+        /// </summary>
+        Dictionary<string, GameScreen> screens = new Dictionary<string, GameScreen>();
+
+        Stack<GameScreen> ScreenStack = new Stack<GameScreen>();
+
+
+    }
+}
